0ccc288b5c6db092c789c5d6a732695592f89503
Nguyễn Quang Huy <nguyenquanghuylt2002@gmail.com>
2023-05-25 17:42:20 +0700

    New slideShow function

--- js/carousel.js
+++ js/carousel.js
@@ -11,3 +11,40 @@ const swiper = new Swiper('.swiper', {
     },
   });
   swiper.autoplay.start();
+
+
+function slideShow(slide){
+  let btnCarouselPrev = document.querySelector(slide.btnControl.prev);
+  let btnCarouselNext = document.querySelector(slide.btnControl.next);
+  let translateX = 0;
+  let translateXVelocity = slide.velocity;
+  let carouselSlider = document.querySelector(slide.wraper);
+  let countSlide = carouselSlider.querySelectorAll(slide.slide).length;
+  let countSlideDisplay = slide.displaySlide;
+  btnCarouselNext.addEventListener('click', () => {
+    translateX -= translateXVelocity;
+    if(translateX < -1*(translateXVelocity*countSlide -translateXVelocity*countSlideDisplay)){
+      translateX = 0;
+    }
+    carouselSlider.style.transform = `translateX(${translateX}px)`;
+  });
+  btnCarouselPrev.addEventListener('click', () => {
+    translateX += translateXVelocity;
+    if(translateX > 0){
+      translateX = -1*(translateXVelocity*countSlide -translateXVelocity*countSlideDisplay);
+    }
+    carouselSlider.style.transform = `translateX(${translateX}px)`;
+  });
+}
+///
+let slide1 = {
+  btnControl: {
+    next:".btn-carousel-next",
+    prev:".btn-carousel-prev"
+  },
+  wraper: ".carousel-category-1",
+  slide: ".product-item",
+  velocity: 238,
+  displaySlide: 5
+}
+slideShow(slide1);
